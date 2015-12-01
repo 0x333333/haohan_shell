c7839b56517a775f3aed4ae5b7a1d78eaddd1ad5
Zhipeng Jiang <jesusjzp@gmail.com>
2015-12-01 17:13:51 +0800

    Update configuration file.

--- config.sh
+++ config.sh
@@ -9,7 +9,29 @@ deb http://ftp.sjtu.edu.cn/ubuntu/ trusty-updates universe restricted multiverse
 deb http://ftp.sjtu.edu.cn/ubuntu/ trusty-proposed universe restricted multiverse main
 deb http://ftp.sjtu.edu.cn/ubuntu/ trusty-backports universe restricted multiverse main" > /etc/apt/sources.list
 
-sudo apt-get Update
+sudo apt-get update
+
+#
+# Set hostname and hosts
+#
+sudo echo "node1" > /etc/hostname
+sudo echo "192.168.6.18 node1
+192.168.6.15 node2
+192.168.6.16 node3" >> /etc/hosts
+
+ssh sk@192.168.6.15
+sudo echo "node2" > /etc/hostname
+sudo echo "192.168.6.18 node1
+192.168.6.15 node2
+192.168.6.16 node3" >> /etc/hosts
+exit
+
+ssh sk@192.168.6.16
+sudo echo "node3" > /etc/hostname
+sudo echo "192.168.6.18 node1
+192.168.6.15 node2
+192.168.6.16 node3" >> /etc/hosts
+exit
 
 #
 # Set ssh
@@ -21,37 +43,15 @@ ssh-copy-id -i ~/.ssh/id_rsa.pub sk@192.168.6.16
 # Go to node2
 ssh sk@node2
 ssh-keygen -t rsa
-ssh-copy-id -i ~/.ssh/id_rsa.pub sk@192.168.6.14
+ssh-copy-id -i ~/.ssh/id_rsa.pub sk@192.168.6.18
 ssh-copy-id -i ~/.ssh/id_rsa.pub sk@192.168.6.16
 exit
 
 # Go to node3
 ssh sk@node3
 ssh-keygen -t rsa
-ssh-copy-id -i ~/.ssh/id_rsa.pub sk@192.168.6.14
+ssh-copy-id -i ~/.ssh/id_rsa.pub sk@192.168.6.18
 ssh-copy-id -i ~/.ssh/id_rsa.pub sk@192.168.6.15
-exit
-
-#
-# Set hostname and hosts
-#
-sudo echo "node1" > /etc/hostname
-sudo echo "192.168.6.14 host1
-192.168.6.15 host2
-192.168.6.16 host3" >> /etc/hosts
-
-ssh sk@192.168.6.15
-sudo echo "node2" > /etc/hostname
-sudo echo "192.168.6.14 host1
-192.168.6.15 host2
-192.168.6.16 host3" >> /etc/hosts
-exit
-
-ssh sk@192.168.6.16
-sudo echo "node3" > /etc/hostname
-sudo echo "192.168.6.14 host1
-192.168.6.15 host2
-192.168.6.16 host3" >> /etc/hosts
 exit
 
 # Install Oracle JDK 8
@@ -59,3 +59,20 @@ sudo apt-get install python-software-properties -y
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install oracle-java8-installer -y
+
+#
+# Spark configuration
+# http://spark.apache.org/downloads.html
+#
+
+# Download pre-build Spark with user-provided Hadoop
+wget http://www.apache.org/dyn/closer.lua/spark/spark-1.5.2/spark-1.5.2-bin-without-hadoop.tgz －O spark.tgz
+tar xvzf spark.tgz && cd spark
+
+# Setup slaves
+# https://spark.apache.org/docs/latest/spark-standalone.html
+cd conf && cp slaves.template slaves
+echo "node3" > slaves
+
+# Start master
+./sbin/start-all.sh
